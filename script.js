44d94039463607abc7b3ce040541bf3c6b677e4d
Balajee Kumar <84266677+balajeekumar07@users.noreply.github.com>
2025-03-01 16:09:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let menuicon = document.getElementById('menu-icon');
+
+let menu = document.querySelector('.nav-list');
+
+
+function showmenu() {
+    menu.style.display = 'block';
+}
+
+function hidemenu() {
+    menu.style.display = 'none';
+}
